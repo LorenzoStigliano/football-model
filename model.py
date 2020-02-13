97c610a0f40f2c2c95d4e7e10c2551772287524a
Lorenzo <s1725018@ed.ac.uk>
2020-02-13 22:27:24 +0000

    started to extract data from CSV file.

--- model.py
+++ model.py
@@ -1,2 +1,16 @@
-# Football model. Using machine learning techniques to predict outcomes of football matches
+# Football model.
+
+# Using machine learning techniques to predict outcomes of football matches
 # using historical data.
+
+# Classification task in order to predict correct outcome (Win, Loss or Draw)
+# Feature vector will have all the stats we want and then we tag it appropriately
+# tag the vector with W, L OR D
+
+# Importing the data.
+import csv
+
+with open('E0.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['Date'], row['Time'])
